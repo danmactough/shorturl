849e931be7bc089448cfdfe58cfd39969d3d69a6
Dan MacTough <danmactough@gmail.com>
2015-03-28 15:06:38 -0400

    Change format of config

--- config-sample.js
+++ config-sample.js
@@ -1,16 +1,38 @@
-exports = module.exports = {
-    port_redirector : 3000
-  , port_main : 3001
-  , port_REPL_redirector : 3010
-  , port_REPL_main : 3011
-  , SessionSecret : ''
-  , BaseUrl : ''
-  , ShortenerUrl : ''
-  , env : 'development'
-  , static: true
-  , db : { uri : {
-           test : ''
-         , development : ''
-         , production : '' }
+var urlFormat = require('url').format;
+var _ = require('lodash');
+
+var config = module.exports = {
+  development: {
+    session_secret: '',
+    db : {
+      uri : 'mongodb://localhost/shorturl-development'
+    }
+  },
+  test: {
+    session_secret: '',
+    db : {
+      uri : 'mongodb://localhost/shorturl-test'
+    }
+  },
+  production: {
+    session_secret: '',
+    db : {
+      uri : 'mongodb://localhost/shorturl'
     }
   }
+};
+
+Object.keys(config).forEach(function (environment) {
+  ['redirector', 'shortener'].forEach(function (service) {
+    var conf = config[environment][service];
+    if (!(conf && typeof conf === 'object')) {
+      conf = config[environment][service] = {};
+    }
+    var defaults = {
+      protocol: 'http',
+      hostname: 'localhost',
+      port: service === 'redirector' ? 3000 : 3001
+    };
+    conf.url = urlFormat(_.defaults(conf, defaults));
+  });
+});
