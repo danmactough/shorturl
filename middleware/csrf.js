f42191ccbb2621be884e37d09ea61a77bf1d80fe
Dan MacTough <danmactough@gmail.com>
2015-04-04 14:44:02 -0400

    Actually fix using apikey in lieu of csrf validation

--- middleware/csrf.js
+++ middleware/csrf.js
@@ -4,16 +4,11 @@
 var csrf = require('csurf');
 
 module.exports = function customCsrf () {
-  return csrf({
-    cookie: false, // use session instead of a cookie
-    value: function (req) {
-      return (req.apikey && req.session._csrf) || // Skip the CSRF check for API requests
-        (req.body && req.body._csrf) ||
-        (req.query && req.query._csrf) ||
-        req.headers['csrf-token'] ||
-        req.headers['xsrf-token'] ||
-        req.headers['x-csrf-token'] ||
-        req.headers['x-xsrf-token'];
-    }
+  var csrfValidate = csrf({
+    cookie: false // use session instead of a cookie
   });
+  return function (req, res, next) {
+    if (req.apikey) next();
+    else csrfValidate(req, res, next);
+  };
 };
